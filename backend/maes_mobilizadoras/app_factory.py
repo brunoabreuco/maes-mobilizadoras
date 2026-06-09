c981c0d39d0d0ba170bf464657f5130832803a16
Bruno de Abreu Correia <b.abreuc@outlook.com>
2026-06-09 11:41:57 -0300

    hotfix: corrige ordem da chamada do debug_mode

--- backend/maes_mobilizadoras/app_factory.py
+++ backend/maes_mobilizadoras/app_factory.py
@@ -18,13 +18,13 @@ BASE_DIR = Path(__file__).parent.parent
 def create_app(test_config: dict | None = None):
     load_dotenv()
 
-    # Ativa modo debug do flask para ativar chave pública de testes
-    debug_mode = os.environ.get('FLASK_DEBUG', '0') == '1'
-    app.config['DEBUG'] = debug_mode
-
     app = Flask(
         __name__, static_folder=str(BASE_DIR.parent / "frontend"), static_url_path=""
     )
+
+    # Ativa modo debug do flask para ativar chave pública de testes
+    debug_mode = os.environ.get('FLASK_DEBUG', '0') == '1'
+    app.config['DEBUG'] = debug_mode
 
     instance_path = Path(app.instance_path)
     instance_path.mkdir(exist_ok=True)
